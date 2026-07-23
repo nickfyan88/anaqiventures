cf15d775dca779206054f182ecccfec7a132577d
nickfyan88 <ean.zee95@gmail.com>
2026-07-23 17:09:35 +0800

    Add Google Analytics script to layout

--- anaqiventures-nextjs/src/app/layout.tsx
+++ anaqiventures-nextjs/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./anaqi-static.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
       metadataBase: new URL("https://anaqiventures.com.my"),
@@ -23,6 +24,8 @@ export default function RootLayout({
                                           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&display=swap"
                                           rel="stylesheet"
                                         />
+                          <Script src="https://www.googletagmanager.com/gtag/js?id=G-9GQ0PDKHDW" strategy="afterInteractive" />
+                          <Script id="ga4-init" strategy="afterInteractive">{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-9GQ0PDKHDW');`}</Script>
                     </head>
                     <body suppressHydrationWarning>{children}
                     </body>
