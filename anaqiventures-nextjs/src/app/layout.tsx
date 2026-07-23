048aa89610d9b888ed93b59939ab2e569da283a2
nickfyan88 <ean.zee95@gmail.com>
2026-07-23 14:25:57 +0800

    Remove GTM script from layout.tsx
    Removed Google Tag Manager script from layout.

--- anaqiventures-nextjs/src/app/layout.tsx
+++ anaqiventures-nextjs/src/app/layout.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 import "./anaqi-static.css";
 
@@ -24,24 +23,8 @@ export default function RootLayout({
                                           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&display=swap"
                                           rel="stylesheet"
                                         />
-                            <Script id="gtm-script" strategy="afterInteractive">
-                                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                                              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                                                        })(window,document,'script','dataLayer','GTM-T2TG2HQ2');`}
-                            </Script>
                     </head>
-                    <body suppressHydrationWarning>
-                            <noscript>
-                                      <iframe
-                                                      src="https://www.googletagmanager.com/ns.html?id=GTM-T2TG2HQ2"
-                                                      height="0"
-                                                      width="0"
-                                                      style={{ display: "none", visibility: "hidden" }}
-                                                    ></iframe>
-                            </noscript>
-                        {children}
+                    <body suppressHydrationWarning>{children}
                     </body>
               </html>
             );
